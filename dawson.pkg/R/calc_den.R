41d8a0535c3611a59780eddfb4fa6beda7424f86
Kari Dawson <you@example.com>
2023-09-26 19:30:16 +0000

    delete

--- dawson.pkg/R/calc_den.R
+++ dawson.pkg/R/calc_den.R
@@ -3,9 +3,3 @@ calc_den <- function (len, width, height ){
   mass <- calc_mass (len)
   density <- mass/volume
   return(density)
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> parent of d0b061e... I modified and broke density
-check 
