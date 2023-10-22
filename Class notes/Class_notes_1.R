57a61998d5bf0b9c03d1fbe1e6490186bd709f7d
karidawson23 <146132051+karidawson23@users.noreply.github.com>
2023-10-22 18:26:54 -0500

    Delete Class notes directory

--- Class notes/Class_notes_1.R
+++
@@ -1,9 +0,0 @@
-str(surveys)
-dim(surveys)
-nrow(surveys)
-ncol(surveys)
-rm(year_type_data) #removes it
-names(surveys)
-summary(surveys)
-head(surveys)
-surveys <- read.csv("data/portal_data_joined.csv")
--- Class notes/Class_notes_3.R
+++
@@ -1,9 +0,0 @@
-surveys %>%
-filter(!is.na(weight)) %>% 
-mutate(weight_kg = weight/1000,
-      weight_kg2 = weight_kg * 2) %>%
-select(weight, weight_kg, weight_kg2)
-
-# ! is the inverse of whatever is in front of it
-# %>%  = command, shift, m
-# to clear console = control + L
